dbffb1d5e2333b9a5308890da6a02fa1adb6e1fe
mohammad riyazuddin <mohammadriyazuddin494@gmail.com>
2025-08-28 21:09:44 +0530

    added the user model

---
+++ src/models/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema =new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required"]
+    }
+},{timestamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified('password')) return next()
+    this.password=await bcrypt.hash(this.password,10)
+})
+
+
+export const User=mongoose.model("User",userSchema);
